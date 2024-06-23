97a58881326df3756b04a2c95b9263a85a50a685
yamaryosan <canda3267@gmail.com>
2024-06-23 19:20:14 +0900

    [create] editorコンポーネントを作成

---
+++ src/app/puzzles/Editor.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import React, { forwardRef, useEffect, useState, useLayoutEffect, useRef, MutableRefObject } from 'react';
+import Quill from 'quill';
+import Delta from 'quill-delta';
+import 'quill/dist/quill.snow.css';
+
+interface EditorProps {
+  readOnly: boolean;
+  defaultValue: any;
+  onTextChange: (delta: any, oldDelta: any, source: string) => void;
+  onSelectionChange: (range: any, oldRange: any, source: string) => void;
+}
+
+/** エディタのコンポーネント
+ * @param {boolean} readOnly - 読み取り専用かどうか
+ * @param {Delta} defaultValue - 初期値
+ * @param {Function} onTextChange - テキストが変更されたときのコールバック
+ * @param {Function} onSelectionChange - 選択範囲が変更されたときのコールバック
+ * @param {Ref} ref - エディタのrefオブジェクト
+ * @returns {React.ReactElement} エディタのコンポーネント
+ */
+// デバッグ用Editorコンポーネント
+const Editor = forwardRef<Quill, EditorProps>(
+  ({ readOnly, defaultValue, onTextChange, onSelectionChange }, ref) => {
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const defaultValueRef = useRef<Delta>(defaultValue);
+    const onTextChangeRef = useRef<typeof onTextChange>(onTextChange);
+    const onSelectionChangeRef = useRef<typeof onSelectionChange>(onSelectionChange);
+    const [quill, setQuill] = useState<Quill | null>(null);
+
+    // レイアウトの変更を検知
+    useLayoutEffect(() => {
+      onTextChangeRef.current = onTextChange;
+      onSelectionChangeRef.current = onSelectionChange;
+    });
+
+    // Quillのインスタンスを取得 (読み取り専用の設定)
+    useEffect(() => {
+      if (ref && typeof ref !== 'function' && ref.current) {
+        ref.current.enable(!readOnly);
+      }
+    }, [ref, readOnly]);
+
+    // エディタの初期化
+    useEffect(() => {
+      const container = containerRef.current;
+      if (!container) {
+        return;
+      }
+      const editorContainer = container?.appendChild(
+        container.ownerDocument.createElement('div'),
+      );
+      import('quill').then((module) => {
+        // Quillのインスタンスを取得(500エラーを解消するため、動的インポートを使用)
+        const Quill = module.default;
+        const quillInstance = new Quill(editorContainer, {
+          theme: 'snow',
+        });
+        setQuill(quillInstance);
+
+        // Quillのインスタンスを設定
+        if (ref) {
+          if (typeof ref === 'function') {
+            ref(quillInstance);
+          } else {
+            ref.current = quillInstance;
+          }
+        }
+
+        // 初期値を設定
+        if (defaultValueRef.current) {
+          quillInstance.setContents(defaultValueRef.current);
+        }
+
+        // テキストが変更されたときのコールバック
+        quillInstance?.on(Quill.events.TEXT_CHANGE, (...args) => {
+          onTextChangeRef.current?.(...args);
+        });
+        // 選択範囲が変更されたときのコールバック
+        quillInstance?.on(Quill.events.SELECTION_CHANGE, (...args) => {
+          onSelectionChangeRef.current?.(...args);
+        });
+      }
+      );
+      // クリーンアップ
+      return () => {
+        if (ref) {
+          if (typeof ref === 'function') {
+            ref(null);
+          } else {
+            ref.current = null;
+          }
+        }
+        container.innerHTML = '';
+      };
+    }, [ref]);
+
+    return <div ref={containerRef}></div>;
+  },
+);
+
+Editor.displayName = 'Editor';
+
+export default Editor;

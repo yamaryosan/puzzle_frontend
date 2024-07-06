6d2daac1c0c0cd3163ea9fc2dc39bf849c201b87
yamaryosan <canda3267@gmail.com>
2024-07-07 00:25:31 +0900

    [add] 新規登録の際、ローカルストレージにメールアドレス保存するようにした

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,7 +11,7 @@ type actionCodeSettings = {
 }
 
 /**
- * 新規ユーザ登録のためにメールを送信する
+ * 新規ユーザ登録のためにメールを送信
  * @param email メールアドレス
  * @param auth Auth
  * @param actionCodeSettings 送信設定
@@ -19,6 +19,7 @@ type actionCodeSettings = {
 async function sendSignInLink(auth: Auth, email: string, actionCodeSettings: actionCodeSettings) {
     try{
         await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+        window.localStorage.setItem("emailForSignIn", email);
     } catch (error) {
         console.error(error);
     }
@@ -27,20 +28,27 @@ async function sendSignInLink(auth: Auth, email: string, actionCodeSettings: act
 export default function App() {
     const auth = getAuth(firebaseApp);
     const actionCodeSettings = {
-        url: "http://localhost:3000/",
+        url: "http://localhost:3000/complete-registration",
         handleCodeInApp: true,
     }
     const [email, setEmail] = useState("");
+    const [isMailSent, setIsMailSent] = useState(false);
 
     const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         sendSignInLink(auth, email, actionCodeSettings);
+        setIsMailSent(true);
     }
 
     return (
         <div>
             <form>
-                <p>メールアドレスを入力してください。リンクが送られます</p>
+                <h1>新規ユーザ登録</h1>
+                {isMailSent ? (
+                    <p>メールを送信しました。リンクをクリックして登録を完了してください</p>
+                ) : (
+                    <p>メールアドレスを入力してください</p>
+                )}
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <button type="submit" onClick={handleSubmit}>登録</button>
             </form>

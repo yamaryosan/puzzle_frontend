0501d5cf4f9fde3eafaa1a6b5c966822a23841b0
yamaryosan <canda3267@gmail.com>
2024-07-06 13:37:40 +0900

    [create] サインアップページを作成

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,48 @@
-export default function Home() {
+'use client';
+
+import { Auth, sendSignInLinkToEmail } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import firebaseApp from "../firebase";
+import React, { useState } from "react";
+
+type actionCodeSettings = {
+    url: string;
+    handleCodeInApp: boolean;
+}
+
+/**
+ * 新規ユーザ登録のためにメールを送信する
+ * @param email メールアドレス
+ * @param auth Auth
+ * @param actionCodeSettings 送信設定
+ */
+async function sendSignInLink(auth: Auth, email: string, actionCodeSettings: actionCodeSettings) {
+    try{
+        await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default function App() {
+    const auth = getAuth(firebaseApp);
+    const actionCodeSettings = {
+        url: "http://localhost:3000/",
+        handleCodeInApp: true,
+    }
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        sendSignInLink(auth, email, actionCodeSettings);
+    }
+
     return (
         <div>
-            <p>新規登録</p>
             <form>
-                <input type="text" placeholder="ユーザー名" />
-                <input type="password" placeholder="パスワード" />
-                <button type="submit">登録</button>
+                <p>メールアドレスを入力してください。リンクが送られます</p>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <button type="submit" onClick={handleSubmit}>登録</button>
             </form>
         </div>
     );

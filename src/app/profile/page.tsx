62a1c3f1cb21281bf5effaf8203dfdc8aa770768
yamaryosan <canda3267@gmail.com>
2024-07-07 00:54:27 +0900

    [add] ユーザ名とメールアドレスをFirebase Authから取得

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import useAuth from './../hooks/useAuth';
+
 
 export default function Profile() {
+    const {user, loading} = useAuth();
+
+    // 読み込み中
+    if (loading) {
+        return <p>読み込み中...</p>;
+    }
+
     return (
         <div>
             <h1>プロフィール</h1>
-            <Image src="/profile.png" alt="プロフィール画像" width={200} height={200} />
-            <p>ユーザー名: ゲスト</p>
-            <p>メールアドレス:</p>
+            <p>ユーザー名: {user?.displayName}</p>
+            <p>メールアドレス: {user?.email}</p>
             <Link href="/profile/[username]/edit" as="/profile/guest/edit">プロフィール編集</Link>
             <Link href="/dashboard">ダッシュボードへ</Link>
         </div>

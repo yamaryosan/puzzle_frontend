3d9a429ec3ac403efac6f2cc97a65ac789e1a42b
yamaryosan <canda3267@gmail.com>
2024-07-07 09:00:52 +0900

    [create] サインアウト用ページを作成

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,6 +9,7 @@ export default function App() {
             <AuthLink href="/theories/create" fallbackHref="/signin">定石作成</AuthLink>
             <AuthLink href="/profile" fallbackHref="/signin">プロフィール</AuthLink>
             <AuthLink href="/temp" fallbackHref="/signin">一時保存中のパズル</AuthLink>
+            <Link href="/signout">ログアウト</Link>
         </div>
     )
 }
---
+++ src/app/signout/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Link from 'next/link';
+import { getAuth, signOut } from 'firebase/auth';
+import firebaseApp from '../firebase';
+import { useEffect } from 'react';
+
+export default function App() {
+    const auth = getAuth(firebaseApp);
+
+    // サインアウト
+    useEffect(() => {
+        signOut(auth);
+    }, []);
+
+    return (
+        <div>
+            <p>サインアウトしました。</p>
+            <Link href="/signin">サインイン</Link>
+        </div>
+    );
+}

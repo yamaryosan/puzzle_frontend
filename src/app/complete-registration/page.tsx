e0a8b35d030748efb3346b4c0d537a46afa090a8
yamaryosan <canda3267@gmail.com>
2024-07-07 09:00:08 +0900

    [add] サインアップ時にユーザ名を設定

--- src/app/complete-registration/page.tsx
+++ src/app/complete-registration/page.tsx
@@ -3,11 +3,12 @@
 import { useState, useEffect } from "react";
 import { getAuth } from "firebase/auth";
 import firebaseApp from "../firebase";
-import { isSignInWithEmailLink, signInWithEmailLink, updatePassword } from "firebase/auth";
+import { isSignInWithEmailLink, signInWithEmailLink, updatePassword, updateProfile } from "firebase/auth";
 
 export default function App() {
     const auth = getAuth(firebaseApp);
     const [email, setEmail] = useState<string>("");
+    const [displayName, setDisplayName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [message, setMessage] = useState<string>("");
 
@@ -26,7 +27,13 @@ export default function App() {
         if (isSignInWithEmailLink(auth, window.location.href)) {
             try {
                 const result = await signInWithEmailLink(auth, email, window.location.href);
+                // プロフィール(ユーザ名)を更新
+                await updateProfile(result.user, {
+                    displayName: displayName,
+                });
+                // パスワードを更新
                 await updatePassword(result.user, password);
+
                 window.localStorage.removeItem("emailForSignIn");
                 setMessage("登録が完了しました");
             } catch (error) {
@@ -35,25 +42,31 @@ export default function App() {
         }
     }
 
-
     return (
         <form>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="メールアドレス"
-            required
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="パスワード"
-            required
-          />
-          <button type="submit" onClick={handleSubmit}>登録を完了する</button>
-          {message && <p>{message}</p>}
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="メールアドレス"
+                required
+            />
+            <input
+                type="text"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+                placeholder="ユーザー名"
+                required
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="パスワード"
+                required
+            />
+            <button type="submit" onClick={handleSubmit}>登録を完了する</button>
+            {message && <p>{message}</p>}
         </form>
-      );
+    );
 };

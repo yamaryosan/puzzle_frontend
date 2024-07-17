77e546d4096610f9587719608afa4f2a9e5e84b6
yamaryosan <canda3267@gmail.com>
2024-07-17 23:06:18 +0900

    [add] Quillエディタでパズルを作成

--- src/app/puzzles/create/page.tsx
+++ src/app/puzzles/create/page.tsx
@@ -1,10 +1,77 @@
-import Link from "next/link";
+'use client';
 
-export default function Home() {
+import React, { useRef, useState } from 'react';
+import Quill from 'quill';
+import Editor from '@/lib/components/Editor';
+
+type Range = {
+    index: number;
+    length: number;
+};
+
+type Change = {
+    ops: any[];
+};
+
+/**
+ * 内容を送信
+ * @param quillRef Quillの参照
+ */
+async function send(quillRef: React.MutableRefObject<Quill | null>) {
+    // Quillの参照が取得できない場合は何もしない
+    if (!quillRef.current) {
+        return;
+    }
+    const contentHtml = quillRef.current?.root.innerHTML;
+
+    const response = await fetch("/api/puzzles", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ contentHtml }),
+    });
+    if (!response.ok) {
+        const error = await response.json();
+        console.error("パズルの作成に失敗: ", error);
+    }
+    const puzzle = await response.json();
+    console.log("パズルの作成に成功: ", puzzle);
+    return puzzle;
+}
+
+export default function App() {
+    const [range, setRange] = useState<Range>();
+    const [lastChange, setLastChange] = useState<Change>();
+    const [readOnly, setReadOnly] = useState(false);
+    const [DeltaClass, setDeltaClass] = useState<any>();
+
+    const quillRef = useRef<Quill | null>(null);
+
+    // Deltaクラスを取得
+    import('quill').then((module) => {
+        const DeltaClass = module.default.import('delta');
+        setDeltaClass(() => DeltaClass);
+    });
+
+    // Deltaクラスが取得できるまでローディング画面を表示
+    if (!DeltaClass) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
-            <Link href="/">パズル作成</Link>
-            <Link href="/puzzles">戻る</Link>
+            <Editor
+            ref={quillRef}
+            readOnly={readOnly}
+            defaultValue={new DeltaClass([{ insert: 'Hello World!' }])}
+            onSelectionChange={setRange}
+            onTextChange={setLastChange}
+            />
+            {/* 内容を送信 */}
+            <button type="button" onClick={() => send( quillRef )}>
+                Send
+            </button>
         </div>
     );
 }

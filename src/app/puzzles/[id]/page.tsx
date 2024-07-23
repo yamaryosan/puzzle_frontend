6fd15f76f079b32cf24b883fb58b0ccc5df67a13
yamaryosan <canda3267@gmail.com>
2024-07-23 22:42:47 +0900

    [add] Viewerを利用しパズルを表示

--- src/app/puzzles/[id]/page.tsx
+++ src/app/puzzles/[id]/page.tsx
@@ -1,14 +1,67 @@
+'use client';
+
 import Link from 'next/link';
+import { getPuzzleById } from '@/lib/api/puzzleapi';
+import { Puzzle } from '@prisma/client';
+import { useEffect, useState, useRef } from 'react';
+import Viewer from '@/lib/components/Viewer';
+import Quill from 'quill';
 
 type PageParams = {
     id: string;
 };
 
-export default function Home({ params }: { params: PageParams }) {
+export default function Page({ params }: { params: PageParams }) {
+    const [puzzle, setPuzzle] = useState<Puzzle | null>(null);
+    const [DeltaClass, setDeltaClass] = useState<any>();
+    const quillRef = useRef<Quill | null>(null);
+
+    useEffect(() => {
+        async function fetchPuzzle() {
+            try {
+                const puzzle = await getPuzzleById(params.id);
+                setPuzzle(puzzle);
+            } catch (error) {
+                console.error("パズルの取得に失敗: ", error);
+            }
+        }
+        fetchPuzzle();
+
+        // Deltaクラスを取得
+        import('quill').then((module) => {
+            const DeltaClass = module.default.import('delta');
+            setDeltaClass(() => DeltaClass);
+        });
+    }, [params.id]);
+
+    useEffect(() => {
+        if (!quillRef.current) {
+            return;
+        }
+        quillRef.current.enable(false);
+        quillRef.current.root.innerHTML = puzzle?.description || "";
+    });
+
+    if (!DeltaClass) {
+        return <div>Loading...</div>
+    }
+
+    if (!puzzle) {
+        return <div>loading...</div>;
+    }
+
     return (
         <div>
-            <h1>パズル{params.id}</h1>
-            <p>詳細</p>
+            <h1>{puzzle?.title}</h1>
+            <p>本文</p>
+            <p>{puzzle.description}</p>
+            <Viewer 
+            readOnly={true}
+            defaultValue={puzzle.description}
+            ref={quillRef}
+            />
+            <p>難易度 : {puzzle.difficulty}</p>
+            <p>お気に入り : puzzle.is_favorite</p>
             <Link href="/puzzles/[id]/edit" as={`/puzzles/${params.id}/edit`}>(管理者のみ)編集</Link>
             <p>解く</p>
             <Link href="/puzzles">戻る</Link>

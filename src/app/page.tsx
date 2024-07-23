79334a8a3b5e3498af1d2ca28f174d754560c28e
yamaryosan <canda3267@gmail.com>
2024-07-23 19:57:09 +0900

    [add] パズル一覧を取得し表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,48 @@
-import Link from "next/link";
+'use client';
 
-export default function Home() {
+import Link from "next/link";
+import { useState } from "react";
+import { Puzzle } from "@prisma/client";
+
+type Puzzles = Puzzle[];
+
+/**
+ * パズル一覧を取得
+ * @returns Promise<Puzzles>
+ */
+async function getPuzzles() {
+  const response = await fetch("/api/puzzles");
+  if (!response.ok) {
+    const error = await response.json();
+    console.error("パズルの取得に失敗: ", error);
+  }
+  const puzzles = await response.json();
+  console.log("パズルの取得に成功: ", puzzles);
+  return puzzles as Puzzles;
+}
+
+export default function Page() {
+  const [puzzles, setPuzzles] = useState<Puzzles | null>(null);
+  // パズル一覧を取得
+  if (!puzzles) {
+    getPuzzles().then((puzzles) => setPuzzles(puzzles));
+  }
+
   return (
     <div>
       <p>難易度で並び替え</p>
       <p>カテゴリー別で並び替え</p>
       <p>ランダムに並び替え</p>
-      <Link href="/puzzles/[id]" as="/puzzles/1">パズル1</Link>
-      <Link href="/puzzles/[id]" as="/puzzles/2">パズル2</Link>
-      <Link href="/puzzles/[id]" as="/puzzles/3">パズル3</Link>
+      <p>パズル一覧</p>
+      <ul>
+        {puzzles?.map((puzzle) => (
+          <li key={puzzle.id}>
+            <Link href={`/puzzles/${puzzle.id}`}>
+              {puzzle.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

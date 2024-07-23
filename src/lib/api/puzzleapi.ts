0d32d6eed24b613bb5e31e502eebbd35c6f3cd17
yamaryosan <canda3267@gmail.com>
2024-07-23 22:40:37 +0900

    [add] IDからパズルを取得する関数を追加

--- src/lib/api/puzzleapi.ts
+++ src/lib/api/puzzleapi.ts
@@ -15,3 +15,25 @@ async function getPuzzles() {
     console.log("パズルの取得に成功: ", puzzles);
     return puzzles as Puzzles;
 }
+
+/**
+ * IDからパズルを取得
+ * @param id パズルID
+ * @returns Promise<Puzzle> パズル
+ */
+async function getPuzzleById(id: string) {
+    const response = await fetch(`/api/puzzles/${id}`);
+    if (!response.ok) {
+        const error = await response.json();
+        console.error("パズルの取得に失敗: ", error);
+    }
+    if (response.status === 404) {
+        console.error("パズルが見つかりません");
+        return null;
+    }
+    const puzzle = await response.json();
+    console.log("パズルの取得に成功: ", puzzle);
+    return puzzle as Puzzle;
+}
+
+export {getPuzzles, getPuzzleById};

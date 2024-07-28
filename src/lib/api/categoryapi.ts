a6ec85af39df65ff814f21e0b7d73c687572bff1
yamaryosan <canda3267@gmail.com>
2024-07-29 07:45:07 +0900

    [add] 新規カテゴリー作成

--- src/lib/api/categoryapi.ts
+++ src/lib/api/categoryapi.ts
@@ -1,3 +1,6 @@
+import { Category } from "@prisma/client";
+type Categories = Category[];
+
 /**
  * カテゴリー一覧を取得
  * @returns Promise<Categories>
@@ -12,3 +15,27 @@ async function getCategories() {
     console.log("カテゴリーの取得に成功: ", categories);
     return categories as Categories;
 }
+
+/**
+ * 新規カテゴリー作成
+ * @param name カテゴリー名
+ * @returns Promise<Category>
+ */
+async function createCategory(name: string) {
+    const response = await fetch("/api/categories", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+    });
+    if (!response.ok) {
+        const error = await response.json();
+        console.error("カテゴリーの作成に失敗: ", error);
+        return;
+    }
+    console.log(`カテゴリー「${name}」の作成に成功`);
+    return response.json() as Promise<Category>;
+}
+
+export { getCategories, createCategory };

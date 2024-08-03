d17b0fc6b6dc55dc8109943d13d31c3ef7096908
yamaryosan <canda3267@gmail.com>
2024-08-04 02:43:06 +0900

    [create] カテゴリー情報を返すGETメソッドを実装

---
+++ src/app/api/categories/[id]/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prismaclient';
+import { Category } from '@prisma/client';
+
+/**
+ * カテゴリー情報を取得
+ * @params req リクエスト
+ * @params params パラメータ
+ * @returns Promise<NextResponse> レスポンス
+ */
+export async function GET(req: NextRequest, {params}: {params: {id: string}}): Promise<NextResponse> {
+    try {
+        const id = parseInt(params.id);
+        if (isNaN(id) || id <= 0) {
+            return NextResponse.json({error: 'Invalid ID'}, {status: 400});
+        }
+        const category = await prisma.category.findUnique({
+            where: {
+                id: id
+            }
+        });
+        if (!category) {
+            return NextResponse.json({error: 'Category not found'}, {status: 404});
+        }
+        return NextResponse.json(category);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({error: error.message, stack: error.stack}, {status: 500});
+        } else {
+            return NextResponse.json({error: 'Unknown error'}, {status: 500});
+        }
+    }
+}

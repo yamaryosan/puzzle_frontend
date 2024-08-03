01f783e18b61a02c6e994cce45442e65008d355b
yamaryosan <canda3267@gmail.com>
2024-08-04 02:43:47 +0900

    [add] ページ内でカテゴリー情報とパズル一覧を取得

--- src/app/categories/[id]/page.tsx
+++ src/app/categories/[id]/page.tsx
@@ -1,16 +1,37 @@
+'use client';
+
 import Link from 'next/link';
+import { getCategoryById, fetchPuzzlesByCategoryId } from '@/lib/api/categoryapi';
+import { Puzzle, Category } from '@prisma/client';
+import { useEffect, useState } from 'react';
 
 type PageParams = {
     id: string;
 };
 
-export default function Home({ params }: { params: PageParams}) {
+export default function Page({ params }: { params: PageParams}) {
+    const [category, setCategory] = useState<Category | null>(null);
+    const [puzzles, setPuzzles] = useState<Puzzle[]>([]);
+
+    // カテゴリー情報を取得
+    useEffect(() => {
+        getCategoryById(params.id).then((category) => {
+            if (!category) return;
+            setCategory(category);
+        });
+    }, [params.id]);
+
+    // カテゴリーに紐づくパズル一覧を取得
+    useEffect(() => {
+        fetchPuzzlesByCategoryId(params.id).then((puzzles) => {
+            if (!puzzles) return;
+            setPuzzles(puzzles);
+        });
+    }, [params.id]);
+
     return (
         <div>
             <p>カテゴリー: {params.id}</p>
-            <p>ランダムに並び替え</p>
-            <p>難易度で並び替え</p>
-            <Link href="/categories">カテゴリー一覧へ</Link>
         </div>
     );
 }

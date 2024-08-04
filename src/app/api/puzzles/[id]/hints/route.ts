d911912220037cbf649aa14dd4fe9a6fe04640f0
yamaryosan <canda3267@gmail.com>
2024-08-05 00:18:14 +0900

    [create] ヒント追加用POSTメソッドを作成

---
+++ src/app/api/puzzles/[id]/hints/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prismaclient";
+
+/**
+ * ヒントを追加
+ * @param req リクエスト
+ * @param params パラメータ
+ */
+export async function POST(req: NextRequest, {params}: {params: {id: string}}) {
+    try {
+        const id = parseInt(params.id);
+
+        // IDが数字でない、または0以下の場合はエラー
+        if (isNaN(id) || id <= 0) {
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+        // パズルが存在しない場合はエラー
+        const puzzle = await prisma.puzzle.findUnique({
+            where: { id },
+        });
+        if (!puzzle) {
+            return NextResponse.json({ error: "Puzzle not found" }, { status: 404 });
+        }
+
+        // ヒントを追加
+        const { hintHtml } = await req.json();
+        await prisma.hint.create({
+            data: {
+                puzzle_id: id,
+                content: hintHtml,
+            },
+        });
+        return NextResponse.json({ message: "Success" });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
+}

67a4dac3665ad00655387c05eeaec6ef698ae6eb
yamaryosan <canda3267@gmail.com>
2024-08-05 22:35:12 +0900

    [add] POSTメソッドを実装

--- src/app/api/approaches/route.ts
+++ src/app/api/approaches/route.ts
@@ -2,6 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prismaclient';
 import { Approaches } from '@prisma/client';
 
+type approachRequest = {
+    title: string;
+    contentHtml: string;
+};
+
 /**
  * 定石一覧を取得する
  * @param req リクエスト
@@ -11,6 +16,35 @@ export async function GET(req: NextRequest) {
     try {
         const approaches = await prisma.approaches.findMany();
         return NextResponse.json(approaches);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
+}
+
+/**
+ * 定石を追加する
+ * @param req リクエスト
+ * @returns レスポンス
+ */
+export async function POST(req: NextRequest) {
+    try {
+        const approachContent: approachRequest = await req.json();
+        const { title, contentHtml } = approachContent;
+        if (!title || !contentHtml) {
+            return NextResponse.json({ error: "Title and contentHtml are required" }, { status: 400 });
+        }
+        const approach: Approaches = await prisma.approaches.create({
+            data: {
+                title,
+                content: contentHtml,
+                puzzle_id: 1,
+            },
+        });
+        return NextResponse.json(approach);
     } catch (error) {
         if (error instanceof Error) {
             return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });

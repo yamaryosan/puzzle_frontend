a754f9f2b358a3e89ce8d1db143a9d262a505c58
yamaryosan <canda3267@gmail.com>
2024-08-05 22:09:35 +0900

    [add] エディタを追加

--- src/app/approaches/create/page.tsx
+++ src/app/approaches/create/page.tsx
@@ -1,11 +1,46 @@
-import Link from "next/link";
+'use client';
 
-export default function Home() {
+import Link from "next/link";
+import Editor from "@/lib/components/Editor";
+import { useState, useEffect } from "react";
+import Quill from 'quill';
+
+type Range = {
+    index: number;
+    length: number;
+};
+
+type Change = {
+    ops: any[];
+};
+
+export default function Page() {
+    const [range, setRange] = useState<Range>();
+    const [lastChange, setLastChange] = useState<Change>();
+    const [DeltaClass, setDeltaClass] = useState<any>();
+
+    useEffect(() => {
+        // Deltaクラスを取得
+        import('quill').then((module) => {
+            const DeltaClass = module.default.import('delta');
+            setDeltaClass(() => DeltaClass);
+        });
+    });
+
+    if (!DeltaClass) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
-            <p>定石作成画面</p>
-            <Link href="/">作成</Link>
-            <Link href="/theories">戻る</Link>
+            <input type="text" placeholder="Title" required/>
+            <Editor 
+            readOnly={false}
+            defaultValue={new DeltaClass([{ insert: 'Hello World!' }])}
+            onSelectionChange={setRange}
+            onTextChange={setLastChange}
+            />
+            <Link href="/approaches">戻る</Link>
         </div>
     );
 }

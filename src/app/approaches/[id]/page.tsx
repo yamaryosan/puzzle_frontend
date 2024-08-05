bf2cfd081d869e2bc0bf65045d3cf4a17593e591
yamaryosan <canda3267@gmail.com>
2024-08-05 23:54:29 +0900

    [add] 定石情報、および定石に紐づく問題を表示

--- src/app/approaches/[id]/page.tsx
+++ src/app/approaches/[id]/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import Link from 'next/link';
-import { getApproach } from '@/lib/api/approachApi';
+import { getApproach, getPuzzlesByApproachId } from '@/lib/api/approachApi';
 import { useEffect, useState } from 'react';
-import { Approach } from '@prisma/client';
+import { Approach, Puzzle } from '@prisma/client';
 import Viewer from '@/lib/components/Viewer';
 
 type PageParams = {
@@ -12,7 +12,9 @@ type PageParams = {
 
 export default function Page({ params }: { params: PageParams }) {
     const [approach, setApproach] = useState<Approach | null>(null);
+    const [puzzles, setPuzzles] = useState<Puzzle[] | null>(null);
 
+    // 定石情報を取得
     useEffect(() => {
         async function fetchApproach() {
             const approach = await getApproach(parseInt(params.id));
@@ -22,6 +24,18 @@ export default function Page({ params }: { params: PageParams }) {
             setApproach(approach);
         }
         fetchApproach();
+    }, []);
+
+    // 定石に紐づく問題情報を取得
+    useEffect(() => {
+        async function fetchPuzzles() {
+            const puzzles = await getPuzzlesByApproachId(parseInt(params.id));
+            if (!puzzles) {
+                return;
+            }
+            setPuzzles(puzzles);
+        }
+        fetchPuzzles();
     }, []);
 
     if (!approach) {
@@ -35,7 +49,18 @@ export default function Page({ params }: { params: PageParams }) {
             readOnly={true}
             defaultValue={approach.content}
             />
-
+            {puzzles?.length === 0 ? <p>問題がありません</p> : ( 
+            <div>
+                <p>この定石に関連する問題</p>
+                <ul>
+                    {puzzles?.map((puzzle) => (
+                        <li key={puzzle.id}>
+                            <Link href={`/puzzles/${puzzle.id}`}>{puzzle.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            )}
             <Link href="/approaches/[id]/edit" as={`/approaches/${params.id}/edit`}>編集</Link>
             <Link href="/approaches">戻る</Link>
         </div>

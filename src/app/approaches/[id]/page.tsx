90c9b2d99b7f0b6fd8613b92fc9f269c3d64ef54
yamaryosan <canda3267@gmail.com>
2024-08-05 23:36:05 +0900

    [add] 各定石表示ページを作成

--- src/app/approaches/[id]/page.tsx
+++ src/app/approaches/[id]/page.tsx
@@ -1,15 +1,43 @@
+'use client';
+
 import Link from 'next/link';
+import { getApproach } from '@/lib/api/approachApi';
+import { useEffect, useState } from 'react';
+import { Approach } from '@prisma/client';
+import Viewer from '@/lib/components/Viewer';
 
 type PageParams = {
     id: string;
 };
 
-export default function Home({ params }: { params: PageParams }) {
+export default function Page({ params }: { params: PageParams }) {
+    const [approach, setApproach] = useState<Approach | null>(null);
+
+    useEffect(() => {
+        async function fetchApproach() {
+            const approach = await getApproach(parseInt(params.id));
+            if (!approach) {
+                return
+            }
+            setApproach(approach);
+        }
+        fetchApproach();
+    }, []);
+
+    if (!approach) {
+        return <div>loading...</div>;
+    }
+
     return (
         <div>
-            <p>定石{params.id}</p>
-            <Link href="/theories/[id]/edit" as={`/theories/${params.id}/edit`}>編集</Link>
-            <Link href="/theories">戻る</Link>
+            <h1>{approach.title}</h1>
+            <Viewer 
+            readOnly={true}
+            defaultValue={approach.content}
+            />
+
+            <Link href="/approaches/[id]/edit" as={`/approaches/${params.id}/edit`}>編集</Link>
+            <Link href="/approaches">戻る</Link>
         </div>
     );
 }

e806352afe69d11dea9093f6bb59031bdf108b2d
yamaryosan <canda3267@gmail.com>
2024-08-05 23:35:50 +0900

    [add] 各定石の情報を取得するメソッドを作成

--- src/lib/api/approachApi.ts
+++ src/lib/api/approachApi.ts
@@ -1,8 +1,8 @@
-import { Approaches } from "@prisma/client";
+import { Approach } from "@prisma/client";
 
 /**
  * 定石一覧を取得する
- * @returns Promise<Approaches[]>
+ * @returns Promise<Approach[]>
  */
 export async function getApproaches() {
     try {
@@ -14,7 +14,28 @@ export async function getApproaches() {
         }
         const approaches = await response.json();
         console.log("定石の取得に成功: ", approaches);
-        return approaches as Approaches[];
+        return approaches as Approach[];
+    } catch (error) {
+        console.error("定石の取得に失敗: ", error);
+    }
+}
+
+/**
+ * 各定石を取得する
+ * @param id 定石ID
+ * @returns Promise<Approach>
+ */
+export async function getApproach(id: number) {
+    try {
+        const response = await fetch(`/api/approaches/${id}`);
+        if (!response.ok) {
+            const error = await response.json();
+            console.error("定石の取得に失敗: ", error);
+            return;
+        }
+        const approach = await response.json();
+        console.log("定石の取得に成功: ", approach);
+        return approach as Approach;
     } catch (error) {
         console.error("定石の取得に失敗: ", error);
     }

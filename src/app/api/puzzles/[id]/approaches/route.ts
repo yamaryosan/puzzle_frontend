20ab2cc815a93d30a7fa64ae75e614552053008d
yamaryosan <canda3267@gmail.com>
2024-08-07 00:53:36 +0900

    [add] パズルに定石を新規追加するメソッド

--- src/app/api/puzzles/[id]/approaches/route.ts
+++ src/app/api/puzzles/[id]/approaches/route.ts
@@ -31,3 +31,49 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
         }
     }
 }
+
+
+/**
+ * パズルに定石を追加
+ * @param req リクエスト
+ * @param params パラメータ
+ * @returns レスポンス
+ */
+export async function POST(req: NextRequest, { params }: {params: {id: string} }): Promise<NextResponse> {
+    try {
+        const id = parseInt(params.id);
+
+        // IDが数字でない、または0以下の場合はエラー
+        if (isNaN(id) || id <= 0) {
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+        
+        // 定石IDを取得
+        const { approachIds } = await req.json();
+
+        // パズルが存在しない場合はエラー
+        const puzzle = await prisma.puzzle.findUnique({
+            where: { id },
+        });
+        if (!puzzle) {
+            return NextResponse.json({ error: "Puzzle not found" }, { status: 404 });
+        }
+
+        // 定石・パズル中間テーブルに追加
+        for (const approachId of approachIds) {
+            await prisma.puzzleApproach.create({
+                data: {
+                    puzzle_id: id,
+                    approach_id: approachId,
+                },
+            });
+        }
+        return NextResponse.json({ message: "Success" });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
+};

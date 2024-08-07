7a9219ab91e319f0bd8253c9ecde9aaa9fcb0dda
yamaryosan <canda3267@gmail.com>
2024-08-07 20:55:27 +0900

    [fix] リンクを修正

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ export default function App() {
             <h1>ダッシュボード</h1>
             <Link href="/puzzles">パズル一覧</Link>
             <AuthLink href="/puzzles/create" fallbackHref="/signin">パズル作成</AuthLink>
-            <AuthLink href="/theories/create" fallbackHref="/signin">定石作成</AuthLink>
+            <AuthLink href="/approaches/create" fallbackHref="/signin">定石作成</AuthLink>
             <AuthLink href="/profile" fallbackHref="/signin">プロフィール</AuthLink>
             <AuthLink href="/temp" fallbackHref="/signin">一時保存中のパズル</AuthLink>
             <Link href="/signout">ログアウト</Link>

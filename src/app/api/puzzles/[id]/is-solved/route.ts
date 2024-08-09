7da8f41e6e3a2bd02077130d44e3e3ea4f48541e
yamaryosan <canda3267@gmail.com>
2024-08-09 18:00:10 +0900

    [add] 正解の場合、ユーザ回答を削除

--- src/app/api/puzzles/[id]/is-solved/route.ts
+++ src/app/api/puzzles/[id]/is-solved/route.ts
@@ -17,10 +17,11 @@ export async function PUT(req: NextRequest, { params }: {params: {id: string}}) 
 
         const {isSolved} = await req.json();
 
-        // 解けたかどうかを更新
+        // 解けたかどうかを更新(正解の場合、回答を削除)
         const puzzle = await prisma.puzzle.update({
             where: { id: id },
             data: {
+                user_answer: isSolved ? "" : undefined,
                 is_solved: isSolved
             },
         });

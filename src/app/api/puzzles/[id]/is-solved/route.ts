7fbaa2a528f0785aba8637423d22c8f3be49f87b
yamaryosan <canda3267@gmail.com>
2024-08-09 10:46:19 +0900

    [create] 解けたかどうかを記録するPUTメソッドを作成

---
+++ src/app/api/puzzles/[id]/is-solved/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * 解けたかどうかを記録
+ * @param req リクエスト
+ * @param params パラメータ
+ */
+export async function PUT(req: NextRequest, { params }: {params: {id: string}}) {
+}

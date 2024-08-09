c313023c49c752e994537c0e3a59c80003bda90c
yamaryosan <canda3267@gmail.com>
2024-08-09 11:04:02 +0900

    [add] メソッドを実装

--- src/app/api/puzzles/[id]/is-solved/route.ts
+++ src/app/api/puzzles/[id]/is-solved/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prismaclient";
 
 /**
  * 解けたかどうかを記録
@@ -6,5 +7,30 @@ import { NextRequest, NextResponse } from "next/server";
  * @param params パラメータ
  */
 export async function PUT(req: NextRequest, { params }: {params: {id: string}}) {
-    
+    try {
+        const id = parseInt(params.id);
+
+        // IDが数字でない、または0以下の場合はエラー
+        if (isNaN(id) || id <= 0) {
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+
+        const {isSolved} = await req.json();
+
+        // 解けたかどうかを更新
+        const puzzle = await prisma.puzzle.update({
+            where: { id: id },
+            data: {
+                is_solved: isSolved
+            },
+        });
+
+        return NextResponse.json(puzzle);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
 }

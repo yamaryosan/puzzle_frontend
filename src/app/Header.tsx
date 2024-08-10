c80bf6995b4bef98d5b6c77a7eb50c8de8937ffe
yamaryosan <canda3267@gmail.com>
2024-08-11 00:38:16 +0900

    [add] スタイリングを変更

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,9 +4,15 @@ import Box from '@mui/material/Box';
 export default function Header() {
     return (
         <header>
-            <Box sx={{ display: 'flex', bgcolor: 'primary.main', padding: '1rem', color: 'text.secondary' }}>
+            <Box sx={{ 
+                display: 'flex', 
+                bgcolor: 'primary.main',
+                padding: '1rem', 
+                color: 'text.secondary',
+                fontSize: '1.1rem',
+                gap: '1rem',
+                }}>
                 <Link href="/">ホーム</Link>
-                <Link href="/dashboard">ダッシュボード</Link>
                 <Link href="/categories">カテゴリー一覧</Link>
                 <Link href="/approaches">定石一覧</Link>
                 <Link href="/favorites">お気に入り</Link>

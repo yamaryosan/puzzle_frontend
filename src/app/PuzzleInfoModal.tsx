4a75575390da22f2e82854becb39c559fbe19ab3
yamaryosan <canda3267@gmail.com>
2024-08-12 09:28:07 +0900

    [create] パズル情報モーダルを作成

---
+++ src/app/PuzzleInfoModal.tsx
@@ -0,0 +1,14 @@
+
+
+/**
+ * パズル情報を表示するモーダル
+ * パズルの見出しをホバーすると表示される
+ * @returns 
+ */
+export default function PuzzleInfoModal() {
+    return (
+        <>
+            <div>パズル情報モーダル</div>
+        </>
+    );
+}

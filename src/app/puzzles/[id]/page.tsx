65be9886145653809b1d3b267ba1ed7062b05d16
yamaryosan <canda3267@gmail.com>
2024-08-18 23:32:57 +0900

    [add] スタイルを追加

--- src/app/puzzles/[id]/page.tsx
+++ src/app/puzzles/[id]/page.tsx
@@ -9,6 +9,8 @@ import Quill from 'quill';
 import Portal from '@/lib/components/Portal';
 import DeleteModal from '@/lib/components/DeleteModal';
 import { Category, Puzzle } from '@prisma/client';
+import { Box, Button } from '@mui/material';
+import { Clear, Delete, Edit, EmojiObjects } from '@mui/icons-material';
 
 
 type PageParams = {
@@ -64,30 +66,101 @@ export default function Page({ params }: { params: PageParams }) {
     };
 
     return (
-        <div>
-            <button onClick={toggleDeleteModal}>
-                {isDeleteModalOpen ? "削除確認ダイアログを閉じる" : "削除確認ダイアログを開く"}
-            </button>
-            <div className="fixed top-20 left-20" id="delete_modal"></div>
+        <Box
+        sx={{
+            padding: "1rem",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+        }}>
+            <div id="delete_modal"></div>
             {isDeleteModalOpen && (
                 <Portal element={document.getElementById("delete_modal")!}>
                     <DeleteModal id={params.id ?? 0} onButtonClick={toggleDeleteModal} />
                 </Portal>
             )}
-            <h1>{puzzle?.title}</h1>
-            <p>本文</p>
-            <p>{puzzle.description}</p>
-            <Viewer 
-            readOnly={true}
-            defaultValue={puzzle.description}
-            ref={quillRef}
-            />
+
+            <h2>{puzzle.title}</h2>
+            <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                paddingY: "0.5rem",
+            }}
+            >
+                <h3>カテゴリー: </h3>
+                <span>{categories?.map(category => (
+                    <span key={category.id}>{category.category.name} </span>
+                ))}</span>
+            </Box>
+
             <p>難易度 : {puzzle.difficulty}</p>
             <p>お気に入り : {puzzle.is_favorite ? 'YES' : 'NO'}</p>
-            <p>カテゴリー : {categories.map((category) => category.category.name).join(", ")}</p>
-            <Link href="/puzzles/[id]/edit" as={`/puzzles/${params.id}/edit`}>(管理者のみ)編集</Link>
-            <Link href="/puzzles/[id]/solve" as={`/puzzles/${params.id}/solve`}>解く</Link>
-            <Link href="/puzzles">戻る</Link>
-        </div>
+
+            <Box
+                sx={{
+                    paddingY: '0.5rem',
+                }}>
+                    <h3>問題文</h3>
+                    <Viewer readOnly={true} defaultValue={puzzle.description}/>
+            </Box>
+
+            <Box
+            sx={{
+                display: "flex",
+                gap: "1rem",
+                marginTop: "1rem",
+                justifyContent: "space-between",
+            }}>
+                <Link href="/puzzles/[id]/solve" as={`/puzzles/${params.id}/solve`} style={{display: "block", width: "20%"}}>
+                <Button
+                sx={{
+                    padding: '1.5rem',
+                    width: '100%',
+                    backgroundColor: 'primary.light',
+                    ":hover": {
+                        backgroundColor: 'primary.main',
+                    },
+                }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', scale: "1.4", color: "black" }}>
+                        <EmojiObjects />
+                        <span>解く</span>
+                    </Box>
+                </Button>
+                </Link>
+
+                <Link href="/puzzles/[id]/edit" as={`/puzzles/${params.id}/edit`} style={{display: "block", width: "20%"}}>
+                <Button
+                sx={{
+                    padding: '1.5rem',
+                    backgroundColor: 'secondary.light',
+                    width: '100%',
+                    ":hover": {
+                        backgroundColor: 'secondary.main',
+                    },
+                }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', scale: "1.4", color: "black" }}>
+                        <Edit />
+                        <span>編集</span>
+                    </Box>
+                </Button>
+                </Link>
+
+                <Button
+                sx={{
+                    padding: '1.5rem',
+                    backgroundColor: 'error.light',
+                    width: '20%',
+                    ":hover": {
+                        backgroundColor: 'error.main',
+                    },
+                }}
+                onClick={toggleDeleteModal}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', scale: "1.4", color: "black" }}>
+                        {isDeleteModalOpen ? <Clear /> : <Delete />}
+                    </Box>
+                </Button>
+            </Box>
+        </Box>
     );
 }

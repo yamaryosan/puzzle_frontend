49fa16c0d50f1684150f82e6390c1f1a7157764e
yamaryosan <canda3267@gmail.com>
2024-08-19 00:37:28 +0900

    [add] アイコンを追加

--- src/app/approaches/page.tsx
+++ src/app/approaches/page.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import { Approach } from '@prisma/client';
 import ApproachCard from '@/lib/components/ApproachCard';
 import { Box, Button } from '@mui/material';
-import { AddCircleOutline } from '@mui/icons-material';
+import { AddCircleOutline, QuizOutlined } from '@mui/icons-material';
 
 export default function Page() {
     const [approaches, setApproaches] = useState<Approach[]>([]);
@@ -29,36 +29,39 @@ export default function Page() {
 
     return (
         <>
-            <h2>定石一覧</h2>
-            {approaches.length === 0 ? <div>定石がありません</div> : null}
-            <ul>
-                {approaches.map(approach => (
-                    <ApproachCard key={approach.id} approach={approach} isActive={approach.id === activeCardId} onClick={() => handleCardClick(approach.id)} />
-                ))}
-            </ul>
-            <Link href="/approaches/create" style={{display: "block"}}>
-            <Box sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                paddingY: "1rem",
-                backgroundColor: "primary.light",
-                cursor: "pointer",
-                ":hover": {
-                    backgroundColor: "primary.main",
-                    transition: "background-color 0.3s",
-                },
-            }}>
-                <Button sx={{
-                    marginX: "0.5rem",
-                    scale: "1.4",
-                    color: "white",
-                    }}>
-                    <AddCircleOutline />
-                    <span>定石作成</span>
-                </Button>
-            </Box>
-            </Link>
+        <h2>
+            <QuizOutlined />
+            定石一覧
+    </h2>
+        {approaches.length === 0 ? <div>定石がありません</div> : null}
+        <ul>
+            {approaches.map(approach => (
+                <ApproachCard key={approach.id} approach={approach} isActive={approach.id === activeCardId} onClick={() => handleCardClick(approach.id)} />
+            ))}
+        </ul>
+        <Link href="/approaches/create" style={{display: "block"}}>
+        <Box sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingY: "1rem",
+            backgroundColor: "primary.light",
+            cursor: "pointer",
+            ":hover": {
+                backgroundColor: "primary.main",
+                transition: "background-color 0.3s",
+            },
+        }}>
+            <Button sx={{
+                marginX: "0.5rem",
+                scale: "1.4",
+                color: "white",
+                }}>
+                <AddCircleOutline />
+                <span>定石作成</span>
+            </Button>
+        </Box>
+        </Link>
         </>
     );
 }

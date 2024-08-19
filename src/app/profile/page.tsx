27794c055a8606990da14d1d0ef2462da1463404
yamaryosan <canda3267@gmail.com>
2024-08-19 21:15:19 +0900

    [add] GoogleAuthのときのユーザ名変更機能

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,10 +4,45 @@ import Image from 'next/image';
 import Link from 'next/link';
 import useAuth from '@/lib/hooks/useAuth';
 import { AccountBoxOutlined } from '@mui/icons-material';
-import ProfileCard from '@/lib/components/ProfileCard';
+import GoogleAuthProfileCard from '@/lib/components/GoogleAuthProfileCard';
+import { useState, useEffect } from 'react';
+import { getAuth, User } from 'firebase/auth';
+import firebaseApp from '@/app/firebase';
+import { Box } from '@mui/material';
 
-export default function Profile() {
-    const {user, authLoading} = useAuth();
+/**
+ * 認証方法を判定
+ * @param user ユーザー
+ * @returns 認証方法
+ */
+function checkAuthProvider(user: User) {
+    if (!user) {
+        throw new Error('ユーザーが見つかりません');
+    }
+    if (user.providerData.length === 0) {
+        throw new Error('認証方法が見つかりません');
+    }
+    const provider = user.providerData[0].providerId;
+    switch (provider) {
+        case 'password':
+            return 'email';
+        case 'google.com':
+            return 'google';
+        default:
+            throw new Error('未対応の認証方法です');
+    }
+}
+
+export default function Page() {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const auth = getAuth(firebaseApp);
+        const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+            setUser(currentUser);
+        });
+        return () => unsubscribe();
+    }, [user]);
 
     // ログインしていない場合
     if (!user) {
@@ -23,18 +58,19 @@ export default function Profile() {
         );
     }
 
-    // 読み込み中
-    if (authLoading) {
-        return <p>読み込み中...</p>;
-    }
-
     return (
-        <div>
+        <Box
+        sx={{
+            padding: "1rem",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+        }}>
             <h2>
                 <AccountBoxOutlined />
                 プロフィール
             </h2>
-            <ProfileCard user={user} />
-        </div>
+            <GoogleAuthProfileCard user={user} />
+        </Box>
     );
 }
---
+++ src/lib/components/GoogleAuthProfileCard.tsx
@@ -0,0 +1,91 @@
+import { getAuth, User, Auth, GoogleAuthProvider, reauthenticateWithPopup } from 'firebase/auth';
+import firebaseApp from '@/app/firebase';
+import { useState, useEffect } from 'react';
+import { updateProfile } from 'firebase/auth';
+import { updateUserInPrisma } from '@/lib/api/userapi';
+import { Box } from '@mui/material';
+
+type GoogleAuthProfileCardProps = {
+    user: User | null;
+}
+
+/**
+ * 再認証
+ * @param auth 認証情報
+ */
+async function reauthenticate(auth: Auth) {
+    try {
+        if (!auth.currentUser) {
+            throw new Error('ユーザーが見つかりません');
+        }
+        const provider = new GoogleAuthProvider();
+        await reauthenticateWithPopup(auth.currentUser, provider);
+    } catch (error) {
+        console.error(error);
+        throw new Error('再認証に失敗しました');
+    }
+}
+
+export default function GoogleAuthProfileCard({ user }: GoogleAuthProfileCardProps) {    
+    const [loading, setLoading] = useState(true);
+    const [username, setUsername] = useState(user?.displayName || "");
+    const [isEditing, setIsEditing] = useState(false);    
+
+    useEffect(() => {
+        if (!user) return;
+        setUsername(user.displayName || "");
+        setLoading(false);
+    }, [user]);
+
+    if (loading) return <p>読み込み中...</p>;
+
+    // 入力欄クリック時のイベント
+    const handleInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    }
+
+    // 編集ボタンクリック時のイベント
+    const handleEditClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setIsEditing(true);
+    }
+
+    // 編集結果確定ボタンクリック時のイベント
+    const handleUpdateClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        try {
+            if (!user) {
+                throw new Error('ユーザーが見つかりません');
+            }
+            await reauthenticate(getAuth(firebaseApp));
+            if (username !== user.displayName) {
+                await updateProfile(user, { displayName: username });
+                // DBのユーザテーブル上の情報も更新
+                await updateUserInPrisma({
+                    firebaseUid: user.uid,
+                    email: user.email,
+                    displayName: username
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+        setIsEditing(false);
+    }
+
+    return (
+        <>
+        {isEditing ? (
+            <>
+            <Box component="input" type="text" value={username} onClick={handleInputClick} onChange={(e) => setUsername(e.target.value)} required
+            sx={{ width: "20%", display: "inline-block", paddingY: "0.5rem", fontSize: "1.5rem" }}/>
+            <button onClick={handleUpdateClick}>更新</button>
+            <p>メールアドレス: {user?.email}</p>
+            </>
+        ) : (
+            <>
+                <p style={{display: "inline-block"}}>{user?.displayName}</p>
+                <button onClick={handleEditClick}>編集</button>
+            <p>メールアドレス: {user?.email}</p>
+            </>
+        )}
+        </>
+    )
+};

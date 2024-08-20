5e7edc0cf100dc0ebd4dd397732963fa8fb60d32
yamaryosan <canda3267@gmail.com>
2024-08-20 23:23:59 +0900

    [fix] 引数名を修正

--- src/app/puzzles/[id]/page.tsx
+++ src/app/puzzles/[id]/page.tsx
@@ -75,7 +75,7 @@ export default function Page({ params }: { params: PageParams }) {
             <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
                 <h2 style={{display: "inline-block"}}>{puzzle.title}</h2>
                     <FavoriteButton
-                        checked={puzzle.is_favorite}
+                        initialChecked={puzzle.is_favorite}
                         puzzleId={params.id}
                         onChange={(checked) => {
                             setPuzzle({ ...puzzle, is_favorite: checked });

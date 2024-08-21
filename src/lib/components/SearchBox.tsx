fadeb8c62d214259eed55c81fd9adb483964eabf
yamaryosan <canda3267@gmail.com>
2024-08-21 22:30:20 +0900

    [add]検索ボックスの作成

---
+++ src/lib/components/SearchBox.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Search } from "@mui/icons-material";
+import { Button, Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { InputBase } from "@mui/material";
+import { searchPuzzles } from "@/lib/api/puzzleapi";
+import { Puzzle } from "@prisma/client";
+import ResultModal from "@/lib/components/ResultModal";
+
+export default function SearchBox() {
+    const [searchText, setSearchText] = useState("");
+    const [searchResults, setSearchResults] = useState<Puzzle[]>([]);
+
+    useEffect(() => {
+        async function fetchPuzzles() {
+            if (searchText.trim().length === 0) {
+                setSearchResults([]);
+                return;
+            }
+            try {
+                const puzzles = await searchPuzzles(searchText) as Puzzle[];
+                setSearchResults(puzzles);
+            } catch (error) {
+                console.error("検索に失敗: ", error);
+            }
+        }
+        fetchPuzzles();
+    }, [searchText]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value);
+    };
+
+    return (
+        <>
+        <Box sx={{ display: "flex", color: "black" }}>
+            <InputBase type="text" placeholder="検索..." value={searchText} onChange={handleChange}
+            sx={{ backgroundColor: "white", padding: "0.25rem", paddingLeft: "0.75rem", borderRadius: "5px", width: "300px" }}
+            />
+            <Button sx={{ color: "white", scale: "1.5" }}>
+                <Search />
+            </Button>
+        </Box>
+        <ResultModal result={searchResults} />
+        </>
+    );
+}

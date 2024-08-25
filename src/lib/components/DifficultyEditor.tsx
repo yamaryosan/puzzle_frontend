5d0578e57995b9a4c8c26f92b38c71c325fbb505
yamaryosan <canda3267@gmail.com>
2024-08-25 23:58:33 +0900

    [create] 難易度入力のためのコンポーネントを作成

---
+++ src/lib/components/DifficultyEditor.tsx
@@ -0,0 +1,17 @@
+import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+
+type DifficultyEditorProps = {
+    value: number;
+    onChange: (value: number) => void;
+};
+
+export default function DifficultEditor({ value, onChange }: DifficultyEditorProps) {
+    return (
+        <>
+            <Rating name="difficulty" value={value} onChange={(_, newValue) => onChange(newValue ?? 1)} 
+                sx={{ fontSize: '4rem' }}
+                />
+        </>
+    );
+}

c187b37f5aff782a748aa2704eed8af07b9a9c77
yamaryosan <canda3267@gmail.com>
2024-08-26 00:42:18 +0900

    [update] インデントを変更

--- src/lib/components/DifficultyEditor.tsx
+++ src/lib/components/DifficultyEditor.tsx
@@ -1,5 +1,4 @@
 import Rating from '@mui/material/Rating';
-import Box from '@mui/material/Box';
 
 type DifficultyEditorProps = {
     value: number;
@@ -9,9 +8,7 @@ type DifficultyEditorProps = {
 export default function DifficultEditor({ value, onChange }: DifficultyEditorProps) {
     return (
         <>
-            <Rating name="difficulty" value={value} onChange={(_, newValue) => onChange(newValue ?? 1)} 
-                sx={{ fontSize: '4rem' }}
-                />
+            <Rating name="difficulty" value={value} onChange={(_, newValue) => onChange(newValue ?? 1)} sx={{ fontSize: '4rem' }} />
         </>
     );
 }

725d72282d18876a2f83f17495118eaecf208bd9
yamaryosan <canda3267@gmail.com>
2024-08-25 23:04:30 +0900

    [create] 各ページを作成

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h2>このサイトについて</h2>
+            <p>このサイトは、Next.jsとMaterial-UIを使って作成したサンプルサイトです。</p>
+        </div>
+    );
+}

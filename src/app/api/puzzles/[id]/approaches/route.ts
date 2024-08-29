015f315fdfee1c7d8982a130e0541559a38c3d97
yamaryosan <canda3267@gmail.com>
2024-08-29 12:32:30 +0900

    [add] 各要素のPUTメソッドを実装

--- src/app/api/puzzles/[id]/approaches/route.ts
+++ src/app/api/puzzles/[id]/approaches/route.ts
@@ -77,3 +77,51 @@ export async function POST(req: NextRequest, { params }: {params: {id: string} }
         }
     }
 };
+
+/**
+ * パズルの定石を更新
+ * @param req リクエスト
+ * @param params パラメータ
+ * @returns レスポンス
+ */
+export async function PUT(req: NextRequest, { params }: {params: {id: string} }): Promise<NextResponse> {
+    try {
+        const id = parseInt(params.id);
+
+        // IDが数字でない、または0以下の場合はエラー
+        if (isNaN(id) || id <= 0) {
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+        
+        // 定石IDを取得
+        const { approachIds } = await req.json();
+
+        // パズルが存在しない場合はエラー
+        const puzzle = await prisma.puzzle.findUnique({
+            where: { id },
+        });
+        if (!puzzle) {
+            return NextResponse.json({ error: "Puzzle not found" }, { status: 404 });
+        }
+
+        // 定石・パズル中間テーブルを更新
+        await prisma.puzzleApproach.deleteMany({
+            where: { puzzle_id: id },
+        });
+        for (const approachId of approachIds) {
+            await prisma.puzzleApproach.create({
+                data: {
+                    puzzle_id: id,
+                    approach_id: approachId,
+                },
+            });
+        }
+        return NextResponse.json({ message: "Success" });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
+};
--- src/app/api/puzzles/[id]/categories/route.ts
+++ src/app/api/puzzles/[id]/categories/route.ts
@@ -76,3 +76,52 @@ export async function POST(req: NextRequest, { params }: {params: {id: string} }
         }
     }
 };
+
+/**
+ * パズルのカテゴリーを更新
+ * @param req リクエスト
+ * @param params パラメータ
+ * @returns レスポンス
+ */
+export async function PUT(req: NextRequest, { params }: {params: {id: string} }): Promise<NextResponse> {
+    try {
+        const id = parseInt(params.id);
+
+        // IDが数字でない、または0以下の場合はエラー
+        if (isNaN(id) || id <= 0) {
+            return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+        }
+        
+        // カテゴリーIDを取得
+        const { categoryIds } = await req.json();
+
+        // パズルが存在しない場合はエラー
+        const puzzle = await prisma.puzzle.findUnique({
+            where: { id },
+        });
+        if (!puzzle) {
+            return NextResponse.json({ error: "Puzzle not found" }, { status: 404 });
+        }
+
+        // カテゴリー・パズル中間テーブルを更新
+        await prisma.puzzleCategory.deleteMany({
+            where: { puzzle_id: id },
+        });
+        for (const categoryId of categoryIds) {
+            await prisma.puzzleCategory.create({
+                data: {
+                    puzzle_id: id,
+                    category_id: categoryId,
+                },
+            });
+        }
+        
+        return NextResponse.json({ message: "Success" });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
+};

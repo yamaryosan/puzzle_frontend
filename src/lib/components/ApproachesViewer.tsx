7e601b244d3421ba25fce616096a0c8d8084333d
yamaryosan <canda3267@gmail.com>
2024-08-31 20:11:00 +0900

    [add] 表示・非表示機能を実装

--- src/lib/components/ApproachesViewer.tsx
+++ src/lib/components/ApproachesViewer.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Quill from "quill";
 
 import DeltaClass from 'quill-delta';
-import { Box, Tabs, Tab } from "@mui/material";
+import { Box, Tabs, Tab, Button } from "@mui/material";
 import TabPanel from "@/lib/components/TabPanel";
 import Viewer from "@/lib/components/Viewer";
 import { getApproachesByPuzzleId } from "@/lib/api/approachApi";
@@ -17,6 +17,7 @@ type ApproachesViewerProps = {
  * @returns 
  */
 export default function ApproachesViewer({ puzzleId }: ApproachesViewerProps) {
+    const [show, setShow] = useState(false);
     const [approaches, setApproaches] = useState<Approach[] | null>(null);
     const [value, setValue] = useState(0);
 
@@ -34,46 +35,55 @@ export default function ApproachesViewer({ puzzleId }: ApproachesViewerProps) {
         setValue(newValue);
     };
 
+    // 表示・非表示の切り替え
+    const handleToggle = () => {
+        setShow(!show);
+    };
+
     return (
         <>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <h3>定石</h3>
+            <Button onClick={handleToggle} sx={{ marginLeft: '1rem' }}>{show ? '非表示' : '表示'}</Button>
+        </Box>
+        {show &&
         <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-        <h3>定石</h3>
-        <Tabs
-            orientation="horizontal"
-            variant="scrollable"
-            value={value}
-            onChange={handleChange}
-            sx={{
-            // タブのスタイル
-            '& .MuiTab-root': {
-                flex: 1, // タブの幅を均等にする
-                maxWidth: 'none',
-                color: 'grey.300',
-                fontSize: '1.2rem',
-                // 非選択時の下線
-                '&::before': {
-                content: '""',
-                position: 'absolute',
-                left: 0,
-                right: 0,
-                bottom: 0,
-                height: '4px',
-                backgroundColor: 'grey.300',
-                transition: 'background-color 0.3s',
+            <Tabs
+                orientation="horizontal"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                sx={{
+                // タブのスタイル
+                '& .MuiTab-root': {
+                    flex: 1, // タブの幅を均等にする
+                    maxWidth: 'none',
+                    color: 'grey.300',
+                    fontSize: '1.2rem',
+                    // 非選択時の下線
+                    '&::before': {
+                    content: '""',
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    height: '4px',
+                    backgroundColor: 'grey.300',
+                    transition: 'background-color 0.3s',
+                    },
                 },
-            },
-            // タブ選択時のスタイル
-            '& .Mui-selected': {
-                color: 'primary.main',
-                fontWeight: 'bold',
-                backgroundColor: 'grey.200',
-                // 選択時の下線
-                '&::before': { backgroundColor: 'primary.main' },
-            },
-            }}>
-                {approaches?.map((approach, index) => (
-                    <Tab label={`定石${index + 1} ${approach.title}`} key={index} />
-                ))}
+                // タブ選択時のスタイル
+                '& .Mui-selected': {
+                    color: 'primary.main',
+                    fontWeight: 'bold',
+                    backgroundColor: 'grey.200',
+                    // 選択時の下線
+                    '&::before': { backgroundColor: 'primary.main' },
+                },
+                }}>
+                    {approaches?.map((approach, index) => (
+                        <Tab label={`定石${index + 1} ${approach.title}`} key={index} />
+                    ))}
             </Tabs>
             {approaches?.map((approach, index) => (
                 <TabPanel value={value} index={index} key={index} >
@@ -81,6 +91,7 @@ export default function ApproachesViewer({ puzzleId }: ApproachesViewerProps) {
                 </TabPanel>
             ))}
         </Box>
+        }
         </>
     )
 }
--- src/lib/components/HintsViewer.tsx
+++ src/lib/components/HintsViewer.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Quill from "quill";
 
 import DeltaClass from 'quill-delta';
-import { Box, Tabs, Tab } from "@mui/material";
+import { Box, Tabs, Tab, Button } from "@mui/material";
 import TabPanel from "@/lib/components/TabPanel";
 import Viewer from "@/lib/components/Viewer";
 import getHintsByPuzzleId from "@/lib/api/hintapi";
@@ -21,6 +21,7 @@ const maxHints = 3;
 export default function HintsViewer({ puzzleId }: HintsViewerProps) {
     const [hints, setHints] = useState<Hint[] | null>(null);
     const [value, setValue] = useState(0);
+    const [show, setShow] = useState(false);
 
     // ヒントを取得
     useEffect(() => {
@@ -36,46 +37,55 @@ export default function HintsViewer({ puzzleId }: HintsViewerProps) {
         setValue(newValue);
     };
 
+    // 表示・非表示の切り替え
+    const handleToggle = () => {
+        setShow(!show);
+    };    
+
     return (
         <>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <h3>ヒント</h3>
+            <Button onClick={handleToggle} sx={{ marginLeft: '1rem' }}>{show ? '非表示' : '表示'}</Button>
+        </Box>
+        {show &&
         <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-        <h3>ヒント</h3>
-        <Tabs
-            orientation="horizontal"
-            variant="scrollable"
-            value={value}
-            onChange={handleChange}
-            sx={{
-            // タブのスタイル
-            '& .MuiTab-root': {
-                flex: 1, // タブの幅を均等にする
-                maxWidth: 'none',
-                color: 'grey.300',
-                fontSize: '1.2rem',
-                // 非選択時の下線
-                '&::before': {
-                content: '""',
-                position: 'absolute',
-                left: 0,
-                right: 0,
-                bottom: 0,
-                height: '4px',
-                backgroundColor: 'grey.300',
-                transition: 'background-color 0.3s',
+            <Tabs
+                orientation="horizontal"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                sx={{
+                // タブのスタイル
+                '& .MuiTab-root': {
+                    flex: 1, // タブの幅を均等にする
+                    maxWidth: 'none',
+                    color: 'grey.300',
+                    fontSize: '1.2rem',
+                    // 非選択時の下線
+                    '&::before': {
+                    content: '""',
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    height: '4px',
+                    backgroundColor: 'grey.300',
+                    transition: 'background-color 0.3s',
+                    },
                 },
-            },
-            // タブ選択時のスタイル
-            '& .Mui-selected': {
-                color: 'primary.main',
-                fontWeight: 'bold',
-                backgroundColor: 'grey.200',
-                // 選択時の下線
-                '&::before': { backgroundColor: 'primary.main' },
-            },
-            }}>
-                {hints?.map((hint, index) => (
-                    <Tab label={`ヒント${index + 1}`} key={index} />
-                ))}
+                // タブ選択時のスタイル
+                '& .Mui-selected': {
+                    color: 'primary.main',
+                    fontWeight: 'bold',
+                    backgroundColor: 'grey.200',
+                    // 選択時の下線
+                    '&::before': { backgroundColor: 'primary.main' },
+                },
+                }}>
+                    {hints?.map((hint, index) => (
+                        <Tab label={`ヒント${index + 1}`} key={index} />
+                    ))}
             </Tabs>
             {hints?.map((hint, index) => (
                 <TabPanel value={value} index={index} key={index} >
@@ -83,6 +93,7 @@ export default function HintsViewer({ puzzleId }: HintsViewerProps) {
                 </TabPanel>
             ))}
         </Box>
+        }
         </>
     )
 }

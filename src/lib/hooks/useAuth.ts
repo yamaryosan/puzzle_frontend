5f39842231bf0050d9af10e7ea8af71c0eed26f6
yamaryosan <canda3267@gmail.com>
2024-09-02 02:57:26 +0900

    [add] ユーザIDを取得

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -2,6 +2,12 @@ import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import firebaseApp from '@/app/firebase';
 
+type UserAuth = {
+    user: User | null;
+    authLoading: boolean;
+    uId?: string | undefined;
+};
+
 /**
  * ユーザーの認証情報を取得するカスタムフック
  * @returns user: ユーザー情報, authLoading: ローディング状態
@@ -9,16 +15,19 @@ import firebaseApp from '@/app/firebase';
 export default function useAuth() {
   const [user, setUser] = useState<User | null>(null);
   const [authLoading, setAuthLoading] = useState(true);
+  const [uId, setUId] = useState<string | null>(null);
 
   useEffect(() => {
     const auth = getAuth(firebaseApp);
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
       setAuthLoading(false);
+        if (user) {
+            setUId(user.uid);
+        }
     });
-
     return () => unsubscribe();
   }, []);
 
-  return { user, authLoading };
+  return { user, authLoading, uId } as UserAuth;
 }

cb4c11c0ef451e31e7b38e190a73f474eef121aa
yamaryosan <canda3267@gmail.com>
2024-09-07 01:04:33 +0900

    [create] メッセージモーダルを作成

---
+++ src/lib/components/MessageModal.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { Box, Paper } from '@mui/material';
+import { ThumbUpAltOutlined } from '@mui/icons-material';
+import { useTransition, animated } from 'react-spring';
+import { config } from 'react-spring';
+
+type MessageModalProps = {
+    message: string;
+};
+
+export default function MessageModal({ message }: MessageModalProps) {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+        }, 3000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
+    const transitions = useTransition(isVisible, {
+        from: { opacity: 0, transform: "translateY(-200%)" },
+        enter: { opacity: 1, transform: "translateY(0%)" },
+        leave: { opacity: 0, transform: "translateY(-400%)" },
+        config: config.stiff,
+    });
+
+    return (
+        transitions((style, item) => item &&
+            <animated.div style={{...style, position: "fixed", top: "1rem", right: "1rem", zIndex: 10 }}>
+                <Paper sx={{ backgroundColor: "white", padding: "1rem", borderRadius: "0.5rem", width: "auto", boxShadow: "0 0 0.5rem rgba(0, 0, 0, 0.2)" }}>
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                        <ThumbUpAltOutlined sx={{ marginRight: "0.5rem" }} />
+                        {message}
+                    </Box>
+                </Paper>
+            </animated.div>
+        )
+    );
+}

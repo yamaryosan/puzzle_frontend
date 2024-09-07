406eb812e09e0f962638a9f0283046ede97239a5
yamaryosan <canda3267@gmail.com>
2024-09-07 22:02:22 +0900

    [add] 編集、削除ボタンを実装

--- src/app/approaches/[id]/page.tsx
+++ src/app/approaches/[id]/page.tsx
@@ -9,6 +9,10 @@ import useAuth from '@/lib/hooks/useAuth';
 import RecommendSignInDialog from '@/lib/components/RecommendSignInDialog';
 import { useSearchParams } from 'next/navigation';
 import MessageModal from '@/lib/components/MessageModal';
+import { Button, Box } from '@mui/material';
+import { Edit, Delete, Clear } from '@mui/icons-material';
+import { useRouter } from 'next/navigation';
+import DeleteModal from '@/lib/components/DeleteModal';
 
 type PageParams = {
     id: string;
@@ -19,9 +23,12 @@ export default function Page({ params }: { params: PageParams }) {
     const [approach, setApproach] = useState<Approach | null>(null);
     const [puzzles, setPuzzles] = useState<Puzzle[] | null>(null);
 
-    const router = useSearchParams();
-    const showCreatedModal = router.get('created') === 'true';
-    const showEditedModal = router.get('edited') === 'true';
+    const searchParams = useSearchParams();
+    const showCreatedModal = searchParams.get('created') === 'true';
+    const showEditedModal = searchParams.get('edited') === 'true';
+
+    const router = useRouter();
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
     // 定石情報を取得
     useEffect(() => {
@@ -47,6 +54,16 @@ export default function Page({ params }: { params: PageParams }) {
         fetchPuzzles();
     }, [userId]);
 
+    // 定石編集画面へ遷移
+    const handleSendButton = () => {
+        router.push(`/approaches/${params.id}/edit`);
+    };
+
+    // 削除確認モーダルの表示切り替え
+    const toggleDeleteModal = () => {
+        setIsDeleteModalOpen(!isDeleteModalOpen);
+    };
+
     if (!user) {
         return <RecommendSignInDialog />;
     }
@@ -63,13 +80,15 @@ export default function Page({ params }: { params: PageParams }) {
         {showEditedModal && (
             <MessageModal message="定石を編集しました" param="edited" />
         )}
-        {user ? (
+        {isDeleteModalOpen && (
+            <DeleteModal target="approach" id={params.id} onButtonClick={toggleDeleteModal} />
+        )}
         <div>
             <h2>{approach.title}</h2>
             <Viewer defaultValue={approach.content ?? ''} />
-            {puzzles?.length === 0 ? <p>この定石に紐づいた問題がありません</p> : ( 
+            {puzzles?.length === 0 ? <p>この定石に紐づくパズルはありません</p> : ( 
             <div>
-                <p>この定石に紐づいた問題</p>
+                <p>この定石に紐づくパズル</p>
                 <ul>
                     {puzzles?.map((puzzle) => (
                         <li key={puzzle.id}>
@@ -79,14 +98,38 @@ export default function Page({ params }: { params: PageParams }) {
                 </ul>
             </div>
             )}
-            <Link href="/approaches/[id]/edit" as={`/approaches/${params.id}/edit`}>編集</Link>
-            <Link href="/approaches">戻る</Link>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', paddingY: '1rem', marginY: '1rem' }}>
+            <Button 
+            sx={{
+                padding: '1.5rem',
+                backgroundColor: 'secondary.light',
+                width: '20%',
+                ":hover": {
+                    backgroundColor: 'secondary.main',
+                }
+            }}
+            onClick={handleSendButton}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', scale: "1.8", color: "black" }}>
+                    <Edit />
+                    <span>編集</span>
+                </Box>
+            </Button>
+            <Button
+            sx={{
+                padding: '1.5rem',
+                backgroundColor: 'error.light',
+                width: '20%',
+                ":hover": {
+                    backgroundColor: 'error.main',
+                },
+            }}
+            onClick={toggleDeleteModal}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', scale: "1.4", color: "black" }}>
+                    {isDeleteModalOpen ? <Clear /> : <Delete />}
+                </Box>
+            </Button>
+        </Box>
         </div>
-        ) : (
-            <div>
-                <RecommendSignInDialog />
-            </div>
-        )}
         </>
     );
 }

c969c1de14153c0227efb5cc7cf28b46242b6f9c
yamaryosan <canda3267@gmail.com>
2024-09-08 13:53:40 +0900

    [create] 立体的に見えるPaperコンポーネントをwrapperとして作成

---
+++ src/lib/components/common/CommonPaper.tsx
@@ -0,0 +1,11 @@
+import { Paper, Box } from '@mui/material';
+
+export default function CommonPaper({ children }: { children: React.ReactNode }) {
+    return (
+        <Paper sx={{ padding: '1rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Box sx={{ width: '100%' }}>
+                {children}
+            </Box>
+        </Paper>
+    );
+}

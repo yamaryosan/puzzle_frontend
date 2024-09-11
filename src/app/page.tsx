e86f73ccd18c6bc5f904f0729d283ea6c159d4f2
yamaryosan <canda3267@gmail.com>
2024-09-11 21:20:09 +0900

    [update] インデントを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,43 +5,50 @@ import { useState } from "react";
 import { Puzzle } from "@prisma/client";
 import { getPuzzles } from "@/lib/api/puzzleapi";
 import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import MessageModal from "@/lib/components/MessageModal";
+import useAuth from "@/lib/hooks/useAuth";
 
 type Puzzles = Puzzle[];
 
 export default function Page() {
-  const [puzzles, setPuzzles] = useState<Puzzles | null>(null);
-  // パズル一覧を取得
-  useEffect(() => {
-    async function fetchPuzzles() {
-      try {
-        const puzzles = await getPuzzles();
-        setPuzzles(puzzles);
-      } catch (error) {
-        console.error("パズルの取得に失敗: ", error);
-      }
-    }
-    fetchPuzzles();
-  }, []);
+    const { user, userId} = useAuth();
 
-  if (!puzzles) {
-    return <div>loading...</div>;
-  }
+    const searchParams = useSearchParams();
+    const passwordReset = searchParams.get("passwordReset") === "true";
+    const [puzzles, setPuzzles] = useState<Puzzles | null>(null);
 
-  return (
-    <div>
-      <p>難易度で並び替え</p>
-      <p>カテゴリー別で並び替え</p>
-      <p>ランダムに並び替え</p>
-      <p>パズル一覧</p>
-      <ul>
-        {puzzles?.map((puzzle) => (
-          <li key={puzzle.id}>
-            <Link href={`/puzzles/${puzzle.id}`}>
-              {puzzle.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    // パズル一覧を取得
+    useEffect(() => {
+        async function fetchPuzzles() {
+            if (userId == null) return;
+            try {
+                const puzzles = await getPuzzles(userId);
+                setPuzzles(puzzles);
+            } catch (error) {
+                console.error("パズルの取得に失敗: ", error);
+            }
+        }
+        fetchPuzzles();
+    }, []);
+
+    return (
+        <div>
+            {passwordReset && ( <MessageModal message="パスワードのリセットが完了しました。" param="passwordReset" /> )}
+            <p>難易度で並び替え</p>
+            <p>カテゴリー別で並び替え</p>
+            <p>ランダムに並び替え</p>
+            <p>パズル一覧</p>
+            <ul>
+                {puzzles?.map((puzzle) => (
+                <li key={puzzle.id}>
+                    <Link href={`/puzzles/${puzzle.id}`}>
+                    {puzzle.title}
+                    </Link>
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
 }

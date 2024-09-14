2e8da8354178c73afda8773d820d810f89d74014
yamaryosan <canda3267@gmail.com>
2024-09-14 16:33:16 +0900

    [update] ユーザ情報のみを返すように変更

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -2,32 +2,27 @@ import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import firebaseApp from '@/app/firebase';
 
-type UserAuth = {
+type Auth = {
     user: User | null;
-    authLoading?: boolean;
-    userId?: string | undefined;
+    authLoading: boolean;
 };
 
 /**
  * ユーザーの認証情報を取得するカスタムフック
- * @returns user: ユーザー情報, authLoading: ローディング状態
+ * @returns ユーザーの認証情報
  */
 export default function useAuth() {
-  const [user, setUser] = useState<User | null>(null);
-  const [authLoading, setAuthLoading] = useState(true);
-  const [userId, setUserId] = useState<string | null>(null);
+    const [user, setUser] = useState<User | null>(null);
+    const [authLoading, setAuthLoading] = useState(true);
 
-  useEffect(() => {
-    const auth = getAuth(firebaseApp);
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-      setAuthLoading(false);
-        if (user) {
-            setUserId(user.uid);
-        }
-    });
-    return () => unsubscribe();
+    useEffect(() => {
+        const auth = getAuth(firebaseApp);
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setAuthLoading(false);
+        });
+        return () => unsubscribe();
   }, []);
 
-  return { user, authLoading, userId } as UserAuth;
+  return { user, authLoading } as Auth;
 }

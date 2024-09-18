49c4a47f8430fd7905ab02362fb7d1161e2f3db3
yamaryosan <canda3267@gmail.com>
2024-09-19 00:00:59 +0900

    [add] サインイン促進のための条件分岐コンポーネントを作成

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,6 +1,7 @@
 import LeftDrawer from "@/app/LeftDrawer";
 import { Box } from "@mui/material";
 import FirebaseUserProvider from "@/lib/components/FirebaseUserProvider";
+import AuthGuardLayout from "@/lib/components/AuthGuardLayout";
 
 export default function Main({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
@@ -9,7 +10,9 @@ export default function Main({ children }: Readonly<{ children: React.ReactNode;
             <Box sx={{ minHeight: '600px', position: 'relative' }}>
                 <LeftDrawer />
                 <Box sx={{ paddingX: '4rem', paddingTop: '2rem' }}>
-                    {children}
+                    <AuthGuardLayout>
+                        {children}
+                    </AuthGuardLayout>
                 </Box>
             </Box>
         </FirebaseUserProvider>

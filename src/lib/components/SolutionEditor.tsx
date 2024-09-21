2cce52887b062f0aa50c9e93ba47758ad3573983
yamaryosan <canda3267@gmail.com>
2024-09-22 01:51:30 +0900

    [create] 正答入力のためのエディタを作成

---
+++ src/lib/components/SolutionEditor.tsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+import Editor from "@/lib/components/Editor";
+import Quill from "quill";
+import Delta from "quill-delta";
+
+type props = {
+    containerRef: React.RefObject<Quill>,
+    onSelectionChange: (range: { index: number, length: number } | null, oldRange: { index: number, length: number } | null, source: string) => void,
+    onTextChange: (delta: Delta | null, oldDelta: Delta | null, source: string) => void,
+}
+
+export default function SolutionEditor({ containerRef, onSelectionChange, onTextChange  }: props) {
+    return (
+        <Box sx={{ paddingY: '0.5rem' }}>
+            <h3>正答</h3>
+            <Editor
+            ref={containerRef}
+            defaultValue={new Delta()}
+            onSelectionChange={onSelectionChange}
+            onTextChange={onTextChange} />
+        </Box>
+    )
+}

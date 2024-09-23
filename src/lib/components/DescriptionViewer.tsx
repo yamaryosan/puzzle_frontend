e13eaa6289a39e0706623888001982030b9ae00b
yamaryosan <canda3267@gmail.com>
2024-09-23 17:15:53 +0900

    [create] 表示用コンポーネントを作成

--- src/lib/components/DescriptionViewer.tsx
+++ src/lib/components/DescriptionViewer.tsx
@@ -4,7 +4,7 @@ import Viewer from '@/lib/components/Viewer';
 export default function DescriptionViewer({ descriptionHtml }: { descriptionHtml: string }) {
     return (
         <Box sx={{ paddingY: '0.5rem' }}>
-            <h3>問題文</h3>
+            <h3>本文</h3>
             <Viewer defaultHtml={descriptionHtml} />
         </Box>        
     )

5461274587fdbdaa227a3e3fb7a27d07990fcac0
yamaryosan <canda3267@gmail.com>
2024-09-23 18:26:18 +0900

    [fix] defaultを追加

--- src/app/puzzles/[id]/solve/page.tsx
+++ src/app/puzzles/[id]/solve/page.tsx
@@ -1,5 +1,5 @@
 import PuzzleSolvePaper from "@/lib/components/PuzzleSolvePaper";
 
-export function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
     return <PuzzleSolvePaper id={params.id} />;
 }

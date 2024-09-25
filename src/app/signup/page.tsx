2c136c8cc9ce81a1a97f1ca37a007f43641c6cce
yamaryosan <canda3267@gmail.com>
2024-09-25 21:45:42 +0900

    [update] エラーメッセージを画面に表示

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -31,9 +31,11 @@ async function sendSignInLink(auth: Auth, email: string, actionCodeSettings: act
         window.localStorage.setItem("emailForSignIn", email);
     } catch (error) {
         if (error instanceof FirebaseError) {
-            // メールアドレスが未入力の場合
-            if (error.code === 'auth/missing-email') {
-                throw new Error('メールアドレスを入力してください');
+            switch (error.code) {
+                case 'auth/missing-email':
+                    throw new Error('メールアドレスは必須です');
+                default:
+                    throw new Error('メールを送信できませんでした');
             }
         }
     }
@@ -47,11 +49,11 @@ async function signUpWithGoogle(auth: Auth) {
     try {
         const provider = new GoogleAuthProvider();
         await signInWithPopup(auth, provider);
-        console.log("Googleアカウントでサインアップしました");
         return true;
     } catch (error) {
-        console.error(error);
-        return false;
+        if (error instanceof FirebaseError) {
+            return false;
+        }
     }
 }
 
@@ -64,6 +66,7 @@ export default function Page() {
     const [email, setEmail] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [isMailSent, setIsMailSent] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     const router = useRouter();
 
@@ -73,7 +76,9 @@ export default function Page() {
             await sendSignInLink(auth, email, actionCodeSettings);
             setIsMailSent(true);
         } catch (error) {
-            console.error(error);
+            if (error instanceof Error) {
+                setError(error.message);
+            }
         }
     }
 
@@ -109,6 +114,7 @@ export default function Page() {
                     ) : (
                         <CommonInputText value={email} elementType="email" onChange={(e) => setEmail(e.target.value)}/>
                     )}
+                    <p className="text-red-500">{error}</p>
                     <Box sx={{ paddingY: '0.5rem' }}>
                         <CommonButton color="primary" onClick={handleSubmit} disabled={isLoading}>
                             <EmailOutlined />

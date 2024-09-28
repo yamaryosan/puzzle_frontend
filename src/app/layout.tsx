989f8afd098f73df8685d0845561382678846a23
yamaryosan <canda3267@gmail.com>
2024-09-28 23:05:10 +0900

    [update] インポート時のパスを変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "@/app/Header";
+import Footer from "@/app/Footer";
 import Main from "@/app/Main";
 import theme from "@/theme";
 import { ThemeProvider, CssBaseline } from "@mui/material";

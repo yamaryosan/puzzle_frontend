0e9b345d19352dfd15d0a3d5fed81a9289fd3450
yamaryosan <canda3267@gmail.com>
2024-09-28 11:53:49 +0900

    [add] デバイスの種類を表示

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import Link from 'next/link';
 import Box from '@mui/material/Box';
-import { Home, Category, Favorite, Search } from '@mui/icons-material';
+import { Home, Category, Favorite } from '@mui/icons-material';
+import { useContext } from 'react';
 import SearchBox from '@/lib/components/SearchBox';
 import FirebaseUserProvider from "@/lib/components/FirebaseUserProvider";
+import DeviceTypeContext from '@/lib/context/DeviceTypeContext';
 
 export default function Header() {
+    const deviceType = useContext(DeviceTypeContext);
     return (
         <header>
+            <p>デバイスの種類: {deviceType}</p>
             <FirebaseUserProvider>
                 <Box sx={{ 
                     display: 'flex', 

065be2369b72ae7a9dd9f509d0ad21d9901cce49
yamaryosan <canda3267@gmail.com>
2024-09-29 18:19:46 +0900

    [add] レスポンシブデザインを追加

--- src/lib/components/DifficultyEditor.tsx
+++ src/lib/components/DifficultyEditor.tsx
@@ -1,5 +1,7 @@
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
+import DeviceTypeContext from '@/lib/context/DeviceTypeContext';
+import { useContext } from 'react';
 
 type DifficultyEditorProps = {
     value: number;
@@ -7,11 +9,13 @@ type DifficultyEditorProps = {
 };
 
 export default function DifficultEditor({ value, onChange }: DifficultyEditorProps) {
+    const deviceType = useContext(DeviceTypeContext);
     return (
         <>
-        <Box sx={{ paddingY: '0.5rem' }}>
+        <Box sx={{ paddingY: '0.5rem', width: '100%' }}>
             <h3>難易度</h3>
-            <Rating name="difficulty" value={value} onChange={(_, newValue) => onChange(newValue ?? 1)} sx={{ fontSize: '4rem' }} />
+            <Rating name="difficulty" value={value} onChange={(_, newValue) => onChange(newValue ?? 1)} 
+            sx={{ fontSize: deviceType === 'mobile' ? '3rem' : '4rem' }} />
         </Box>
         </>
     );

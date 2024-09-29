15de2ef9cc0d53d62ac48c538ee4d829329f2390
yamaryosan <canda3267@gmail.com>
2024-09-29 22:33:28 +0900

    [update] h3をh4に変更

--- src/lib/components/CategoryShowPart.tsx
+++ src/lib/components/CategoryShowPart.tsx
@@ -4,7 +4,7 @@ import { Category } from '@prisma/client';
 export default function CategoryShowPart({ categories }: { categories: Category[] }) {
     return (
         <Box sx={{ display: "flex", alignItems: "center", paddingY: "0.5rem" }}>
-            <h3>カテゴリー:</h3>
+            <h4>カテゴリー:</h4>
             {categories?.length === 0 && <span>未設定</span>}
             <span>{categories?.map(category => (
                 <span key={category.id}>{category.name}</span>
--- src/lib/components/DescriptionViewer.tsx
+++ src/lib/components/DescriptionViewer.tsx
@@ -4,7 +4,7 @@ import Viewer from '@/lib/components/Viewer';
 export default function DescriptionViewer({ descriptionHtml }: { descriptionHtml: string }) {
     return (
         <Box sx={{ paddingY: '0.5rem' }}>
-            <h3>本文</h3>
+            <h4>説明:</h4>
             <Viewer defaultHtml={descriptionHtml} />
         </Box>
     )

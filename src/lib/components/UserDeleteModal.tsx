5f22aad785f0979e1fae6014eff8dce8f0dfd20a
yamaryosan <canda3267@gmail.com>
2024-09-30 07:56:14 +0900

    [update] 退会ボタン有効化までの秒数を変更

--- src/lib/components/UserDeleteModal.tsx
+++ src/lib/components/UserDeleteModal.tsx
@@ -104,11 +104,11 @@ export default function UserDeleteModal({ onButtonClick }: DeleteModalProps) {
         };
     }, [onButtonClick]);
 
-    // 5秒後に退会ボタンを有効化
+    // 3秒後に退会ボタンを有効化
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsDeleteButtonDisabled(false);
-        }, 5000);
+        }, 3000);
         return () => clearTimeout(timer);
     }, []);
 

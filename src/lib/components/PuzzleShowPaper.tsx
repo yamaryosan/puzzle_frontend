0b63d607aa82897584bfa9189b47aae9bf890757
yamaryosan <canda3267@gmail.com>
2024-09-29 22:33:11 +0900

    [add] レスポンシブデザインを追加

--- src/lib/components/PuzzleShowPaper.tsx
+++ src/lib/components/PuzzleShowPaper.tsx
@@ -19,7 +19,7 @@ import MessageModal from '@/lib/components/MessageModal';
 import CommonButton from '@/lib/components/common/CommonButton';
 import CategoryShowPart from '@/lib/components/CategoryShowPart';
 import DescriptionViewer from '@/lib/components/DescriptionViewer';
-
+import DeviceTypeContext from '@/lib/context/DeviceTypeContext';
 
 export default function PuzzleShowPaper({ id }: { id: string }) {
     const user = useContext(FirebaseUserContext);
@@ -30,11 +30,13 @@ export default function PuzzleShowPaper({ id }: { id: string }) {
     const [isLoading, setIsLoading] = useState(true);
 
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+    const deviceType = useContext(DeviceTypeContext);
+
     // モーダルの表示
     const searchParams = useSearchParams();
     const showCreatedModal = searchParams.get('created') === 'true';
     const showEditedModal = searchParams.get('edited') === 'true';
-
 
     // 削除確認ダイアログの開閉
     const toggleDeleteModal = () => {
@@ -84,7 +86,8 @@ export default function PuzzleShowPaper({ id }: { id: string }) {
         )}
 
         <Paper sx={{ padding: "1rem" }}>
-            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+            {deviceType === "mobile" && (
+            <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column", justifyContent: "space-between" }}>
                 <h2 style={{display: "inline-block"}}>{puzzle?.title}</h2>
                 <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
                     <CompletionStatusIcon isSolved={puzzle?.is_solved ?? false} />
@@ -96,16 +99,56 @@ export default function PuzzleShowPaper({ id }: { id: string }) {
                         }}/>
                 </Box>
             </Box>
+            )}
+
+            {deviceType === "desktop" && (
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <h2>{puzzle?.title}</h2>
+                <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+                    <CompletionStatusIcon isSolved={puzzle?.is_solved ?? false} />
+                    <FavoriteButton
+                        initialChecked={puzzle?.is_favorite ?? false}
+                        puzzleId={id}
+                        onChange={(checked) => {
+                            setPuzzle({ ...puzzle!, is_favorite: checked });
+                        }}/>
+                </Box>
+            </Box>
+            )}
 
             <CategoryShowPart categories={categories} />
             
             <Box sx={{ display: "flex", alignItems: "center", paddingY: "0.5rem" }}>
-                <h3>難易度: </h3>
+                <h4>難易度:</h4>
                 <DifficultViewer value={puzzle?.difficulty ?? 0} />
             </Box>
 
             <DescriptionViewer descriptionHtml={puzzle?.description ?? ""} />
 
+            {deviceType === "mobile" && (
+            <Box sx={{ display: "flex", gap: "0.5rem", flexDirection: "column" }}>
+                <Link href="/puzzles/[id]/solve" as={`/puzzles/${id}/solve`} style={{display: "block"}}>
+                <CommonButton color="primary" onClick={() => {}} width="100%" >
+                    <EmojiObjects />
+                    解く
+                </CommonButton>
+                </Link>
+
+                <Link href="/puzzles/[id]/edit" as={`/puzzles/${id}/edit`} style={{display: "block"}}>
+                <CommonButton color="secondary" onClick={() => {}} width="100%" >
+                    <Edit />
+                    編集
+                </CommonButton>
+                </Link>
+
+                <CommonButton color="error" onClick={toggleDeleteModal} >
+                    <Delete />
+                    削除
+                </CommonButton>
+            </Box>
+            )}
+
+            {deviceType === "desktop" && (
             <Box sx={{ display: "flex", gap: "1rem", marginTop: "1rem", justifyContent: "space-between" }}>
                 <Link href="/puzzles/[id]/solve" as={`/puzzles/${id}/solve`} style={{display: "block", width: "30%"}}>
                 <CommonButton color="primary" onClick={() => {}} width="100%" >
@@ -126,6 +169,7 @@ export default function PuzzleShowPaper({ id }: { id: string }) {
                     削除
                 </CommonButton>
             </Box>
+            )}
 
             <div id="delete_modal"></div>
             {isDeleteModalOpen && (

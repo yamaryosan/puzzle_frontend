5d0dc33d2524e0ef03f5c9b229cf47628f84e16a
yamaryosan <canda3267@gmail.com>
2024-09-30 23:41:38 +0900

    [add] パズルをランダムに取得するメソッドを実装

---
+++ src/app/api/puzzles/random/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prismaclient";
+
+/**
+ * ランダムにパズルを取得する
+ * @param req リクエスト
+ */
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    const { searchParams } = new URL(req.url);
+    const user_id = searchParams.get("userId");
+    if (!user_id) {
+        throw new Error("ユーザIDが指定されていません");
+    }
+    const count = searchParams.get("count");
+    if (!count) {
+        throw new Error("取得するパズルの数が指定されていません");
+    }
+    try {
+        // パズルをランダムに取得
+        const puzzles = await prisma.puzzle.findMany({
+            where: { user_id },
+            orderBy: { id: 'desc' },
+            take: parseInt(count),
+        });
+        return NextResponse.json(puzzles);
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+        } else {
+            return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+        }
+    }
+}

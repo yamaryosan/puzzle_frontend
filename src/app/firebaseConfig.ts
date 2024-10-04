ab52850120e6fb713fe9be028251fcf9f3a550c2
yamaryosan <canda3267@gmail.com>
2024-10-05 01:17:33 +0900

    [add] ステージング用環境変数設定を追加

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -16,6 +16,15 @@ const prodConfig = {
     appId: process.env.NEXT_PUBLIC_PROD_FIREBASE_APP_ID,
 };
 
-const firebaseConfig = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+const stagingConfig = {
+    apiKey: process.env.NEXT_PUBLIC_STAGING_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_STAGING_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_STAGING_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STAGING_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_STAGING_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_STAGING_FIREBASE_APP_ID,
+};
+
+const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_ENV === 'production' ? prodConfig : process.env.NEXT_PUBLIC_FIREBASE_ENV === 'staging' ? stagingConfig : devConfig;
 
 export default firebaseConfig;

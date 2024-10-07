02b6fd096195ac7a3314334c88b12a28cf617564
yamaryosan <canda3267@gmail.com>
2024-10-07 23:11:31 +0900

    [add] 戻るボタンを追加

--- src/lib/components/ApproachNotFound.tsx
+++ src/lib/components/ApproachNotFound.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+import CommonButton from "@/lib/components/common/CommonButton";
+import { UndoOutlined } from "@mui/icons-material";
+
 export default function ApproachNotFound() {
-    return <div>定石が見つかりません。</div>;
+    return (
+        <>
+        <p>定石が見つかりません。</p>
+        <Link href="/approaches">
+            <CommonButton color="primary" onClick={() => {}}>
+            <UndoOutlined />
+            <span>定石一覧へ</span>
+            </CommonButton>
+        </Link>
+        </>
+    )
 }
--- src/lib/components/PuzzleNotFound.tsx
+++ src/lib/components/PuzzleNotFound.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+import CommonButton from "@/lib/components/common/CommonButton";
+import { UndoOutlined } from "@mui/icons-material";
+
 export default function PuzzleNotFound() {
-    return <div>パズルが見つかりません。</div>;
+    return (
+        <>
+        <p>パズルが見つかりません。</p>
+        <Link href="/puzzles">
+            <CommonButton color="primary" onClick={() => {}}>
+            <UndoOutlined />
+            <span>パズル一覧へ</span>
+            </CommonButton>
+        </Link>
+        </>
+    )
 }

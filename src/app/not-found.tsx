a94d961c7133ba877ad41ced0c2a2e273b8648b4
yamaryosan <canda3267@gmail.com>
2024-10-07 23:12:50 +0900

    [add] 戻るボタンを追加

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import Link from "next/link";
+import CommonButton from "@/lib/components/common/CommonButton";
+import { UndoOutlined } from "@mui/icons-material";
+
 export default function NotFound() {
     return (
         <>
         <h2>404 Not Found</h2>
         <p>ページが見つかりません。</p>
+        <Link href="/">
+            <CommonButton color="primary" onClick={() => {}}>
+            <UndoOutlined />
+            <span>ホームへ</span>
+            </CommonButton>
+        </Link>
         </>
     );
 }

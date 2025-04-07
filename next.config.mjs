bc2145753faed158ea92995db83ba864928143b4
yamaryosan <canda3267@gmail.com>
2025-04-07 18:38:27 +0900

    clean: prettierでフォーマッティング

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
-    return [
-        {
-        source: '/(.*)',
-        headers: [
+        return [
             {
-                key: 'Cross-Origin-Opener-Policy',
-                value: 'same-origin-allow-popups',
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin-allow-popups",
+                    },
+                ],
             },
-        ],
-        },
-    ];
+        ];
     },
 };
 

aa93b8b836d048c3772741d627a5a074d893408e
yamaryosan <canda3267@gmail.com>
2025-04-07 14:13:21 +0900

    debug: コンソールにメッセージを表示

--- src/lib/components/EmailAuthProfileCard.tsx
+++ src/lib/components/EmailAuthProfileCard.tsx
@@ -119,13 +119,13 @@ export default function EmailAuthProfileCard({
         }
 
         console.log(user.email);
-        console.log(process.env.PORTFOLIO_CHECK_EMAIL1);
-        console.log(process.env.PORTFOLIO_CHECK_EMAIL2);
+        console.log(process.env.NEXT_PUBLIC_PORTFOLIO_CHECK_EMAIL1);
+        console.log(process.env.NEXT_PUBLIC_PORTFOLIO_CHECK_EMAIL2);
 
         // ポートフォリオチェック用アカウントの場合は更新できない
         if (
-            user.email === process.env.PORTFOLIO_CHECK_EMAIL1 ||
-            user.email === process.env.PORTFOLIO_CHECK_EMAIL2
+            user.email === process.env.NEXT_PUBLIC_PORTFOLIO_CHECK_EMAIL1 ||
+            user.email === process.env.NEXT_PUBLIC_PORTFOLIO_CHECK_EMAIL2
         ) {
             setGeneralError("このアカウントは更新できません");
             return;
